f6edc3120f83263c1b4c265a5bfe770fb6646b98
Robin Lan <bjtulan@gmail.com>
2019-04-02 09:25:33 +0800

    梯度下降

--- 040/TemperatureControl.py
+++ 040/TemperatureControl.py
@@ -12,7 +12,25 @@ def ReadData():
     else:
         return None, None
 
+def Gradient(X, Y):
+    eta = 0.1
+    w, b = 0.0, 0.0
+    num_example = X.shape[0]
+    for i in range(num_example):
+        x = X[i]
+        y = Y[i]
+        z = w * x + b
+        dz = z - y
+        db = dz
+        dw = dz * x
+        w = w - eta * dw
+        b = b = eta * db
+
+    print(w, b)
+    return w, b
+
 if __name__ == '__main__':
     X, Y = ReadData()
-    plt.plot(X, Y, '.')
-    plt.show();
+    #plt.plot(X, Y, '.')
+    #plt.show();
+    Gradient(X, Y)
